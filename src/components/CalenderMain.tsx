dcefb309fdde6bc9e6e9bae3defb50271338951d
Vishnu R <vishnurelampal@gmial.com>
2026-02-13 22:47:43 +0530

    Project structured and helper functions added

---
+++ src/components/CalenderMain.tsx
@@ -0,0 +1,5 @@
+const CalenderMain = () => {
+  return <div>Calender Main</div>;
+};
+
+export default CalenderMain;
---
+++ src/utils/DateUtils.ts
@@ -0,0 +1,8 @@
+function numberOfDaysInMonth(year: number, month: number) {
+  return new Date(year, month + 1, 0).getDate();
+}
+
+function firstDayOfMonth(year: number, month: number) {
+  return new Date(year, month, 1).getDay();
+};
+export { numberOfDaysInMonth, firstDayOfMonth };
